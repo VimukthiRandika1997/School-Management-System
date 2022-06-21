8b58efdc3e683aaa90e87ec34c33bd2af6fb3d16
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-06-21 09:09:24 +0530

    Fetching databases feature added

--- Back-End/connect.js
+++ Back-End/connect.js
@@ -1,8 +1,32 @@
 const {MongoClient} = require('mongodb');
 
 async function main(){
-    const URI = 
+    const URI = "mongodb+srv://vithushan:vithu123@cluster0.lq7ul.mongodb.net/?retryWrites=true&w=majority"
 
     const client = new MongoClient(URI);
-    
+
+    try{
+        await client.connect();
+        await listDatabases(client);
+
+    }catch(e) {
+        console.error(e);
+
+    // Close the connection
+    } finally {
+        await client.close();
+    }
+
+}
+
+main().catch(console.error);
+
+// fetching the databases from the cluster
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases");
+    databasesList.databases.forEach(db => {
+        console.log(`-${db.name}`);
+    });
 }
